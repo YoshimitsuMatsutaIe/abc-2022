eb7c9aaba532372e6a0686bb82c136ecb9c7476b
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2022-02-20 01:39:23 +0900

    other

---
+++ other/matrix_1.c
@@ -0,0 +1,12 @@
+void matrix_1(int *out)
+{
+    out[0] = 1;
+    out[1] = 2;
+    out[2] = 3;
+    out[3] = 4;
+    out[4] = 5;
+    out[5] = 6;
+    out[6] = 7;
+    out[7] = 8;
+    out[8] = 9;
+}
